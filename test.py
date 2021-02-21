413fe761b56c9d6799cd5e96e786cee88e9f6ded
Sameeul Bashir Samee <sameeul@gmail.com>
2021-02-21 18:08:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from rcc import Rcc
-rcc_obj = Rcc.instance()
-test_str = "alaska"
-print(rcc_obj.encode(test_str))
-rcc_obj_2 = Rcc.instance()
-print(rcc_obj_2.encode(test_str))
+rcc = Rcc.instance()
+test_str = "alaska is a funny pl\\ace"
+encoded_str = rcc.encode(test_str)
+print(encoded_str)
